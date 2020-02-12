2221e1a56ea34e112ee141d0796c5b414b963669
bloriot97 <bloriot97@gmail.com>
2020-02-12 17:49:47 -0500

    Add get models

---
+++ getModels.sh
@@ -0,0 +1,3 @@
+mkdir models
+curl -o models/pose_iter_440000.caffemodel http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/coco/pose_iter_440000.caffemodel
+curl -o models/pose_iter_160000.caffemodel http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel
